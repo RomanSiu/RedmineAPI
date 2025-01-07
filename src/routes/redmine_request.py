3a61e42dc8cd7121568da7e499214b7d30eed283
RomanSiu <siusiailoroman@gmail.com>
2025-01-07 11:35:27 +0200

    bug fix

--- src/routes/redmine_request.py
+++ src/routes/redmine_request.py
@@ -13,9 +13,9 @@ FILE_PATH = Path("src/xlsx_files/Issues info.xlsx")
 @router.get("/issues_info")
 async def issues_info(contract_num: str = None, project_stage: str | int = None,
                       time_from: str = None, time_to: str = None):
-    await get_issues_info(contract_num=contract_num, project_stage=project_stage,
+    result = await get_issues_info(contract_num=contract_num, project_stage=project_stage,
                           time_from=time_from, time_to=time_to)
-    return {'message': 'Successful'}
+    return result
 
 
 @router.get("/download_excel", response_class=FileResponse)
