e87ccaccadb6351cc9a0f6d4290d3373da163146
RomanSiu <siusiailoroman@gmail.com>
2025-02-12 10:11:12 +0200

    add time_entry_date field

--- src/utils/redminereq.py
+++ src/utils/redminereq.py
@@ -173,6 +173,7 @@ def get_time_entries(issue, time_from, time_to):
             time_entries_data['name'] = name
             time_entries_data['user_id'] = user_id
             time_entries_data['real_hours'] = time_entry.hours
+            time_entries_data['time_entry_date'] = time_entry.created_on.strftime("%d-%m-%Y")
             time_entries_data['updated_data'] = time_entry.updated_on.strftime("%d-%m-%Y")
             time_entries_data_dict.append(time_entries_data)
             # if name not in time_entries_data_dict:
